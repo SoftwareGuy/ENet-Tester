ccf87b6e1d144ed99feb7ef510c16f5c57127613
Matt Coburn <coburn@dandere.net>
2020-09-18 14:28:34 +1000

    First Release
    All good to go

---
+++ Program.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using ENet;
+
+namespace ENetTester
+{
+    class Program
+    {
+        static string serverAddress = string.Empty;
+        static int port = 7777;
+        static int maxChannels = 1;
+        static int maxClients = 50;
+        static bool killSwitch = false;
+        static string dummyData = "Never gonna give you up. Never gonna let you down. Never gonna run around and desert you";
+
+        static void Main(string[] args)
+        {
+            // Thread references
+            Thread serverThread = new Thread(DoEnetServerWork);
+            Thread clientThread = new Thread(DoEnetClientWork);
+
+            // Is this server mode?
+            bool isServer = false;
+
+            // Advertisements
+            Console.WriteLine("ENet Testing Application, written by Matt Coburn (SoftwareGuy/Coburn)");
+            Console.WriteLine("Report bugs and submit PRs at https://github.com/SoftwareGuy/ENet-Tester");
+            Console.WriteLine();
+            Console.WriteLine("This application is licensed under the MIT license");
+            Console.WriteLine("and comes with no warranty. Read the license file.");
+            Console.WriteLine();
+
+            Console.Write("Command Line: ");
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.Write($"{args[i]} ");
+            }
+            Console.WriteLine();
+
+            // Check args.
+            if (args.Length > 1)
+            {
+                switch (args[0])
+                {
+                    case "client":
+                        Console.WriteLine("CLIENT MODE ENABLED");
+                        isServer = false;
+                        break;
+
+                    case "server":
+                        Console.WriteLine("SERVER MODE ENABLED");
+                        isServer = true;
+                        break;
+
+                    default:
+                        PrintUsageInfo();
+                        Environment.Exit(1);
+                        break;
+                }
+
+                // Check if we've got an address
+                if (args.Length >= 2)
+                {
+                    Console.WriteLine($"Using supplied address to connect/bind to: {args[1]}");
+                    serverAddress = args[1];
+                } else
+                {
+                    Console.WriteLine($"ERROR: No IP address specified!");
+                    PrintUsageInfo();
+                    Environment.Exit(1);
+                }
+
+                // Check if we've got an port
+                if (args.Length == 3)
+                {
+                    Console.WriteLine($"Using supplied port to connect/bind to: {args[1]}");
+                    if (int.TryParse(args[2], out int parsedPort))
+                    {
+                        if (parsedPort <= 0 || parsedPort > 65535)
+                        {
+                            Console.WriteLine($"WARN: Port out of range, using sane default of {port}.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Using supplied address to connect/bind to: {args[2]}");
+                            port = parsedPort;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"WARN: Port was bogus, using sane default of {port}.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"WARN: Port was not supplied, using sane default of {port}.");
+                }
+            }
+            else
+            {
+                PrintUsageInfo();
+                Environment.Exit(1);
+            }
+
+            // ---
+            Console.WriteLine();
+            Console.WriteLine("At any time, press ESC to shutdown");
+            Console.WriteLine();
+            // ---
+
+            // Initialize ENet
+            Library.Initialize();
+
+            if (isServer)
+            {
+                Console.WriteLine("Starting Server Thread...");
+                serverThread.Start();
+            }
+            else
+            {
+                Console.WriteLine("Starting Client Thread...");
+                clientThread.Start();
+            }
+
+            while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape))
+            {
+                // Take a nap.
+                ;
+            }
+
+            Console.WriteLine("Shutting down... this might take a moment...");
+            killSwitch = true;
+
+            if (isServer)
+            {
+                if (serverThread.IsAlive)
+                {
+                    serverThread.Join();
+                }
+            }
+            else
+            {
+                if(clientThread.IsAlive)
+                {
+                    clientThread.Join();
+                }
+            }
+
+            // Deinitialize ENet
+            Library.Deinitialize();
+            Environment.Exit(0);
+        }
+
+        static void PrintUsageInfo()
+        {
+            Console.WriteLine("ERROR: Bad usage of this application.");
+            Console.WriteLine("Usage: ENetTester [server/client] [address] [port]");
+            Console.WriteLine("Where:");
+            Console.WriteLine("[server/client] refers to the mode you want. You need to specify either server or client.");
+            Console.WriteLine("[address] is the IPv4/IPv6 address you wish to bind the server to or connect the client to.");
+            Console.WriteLine("[port] is the port you wish to bind or connect to.");
+            Console.WriteLine("");
+        }
+
+
+        // -- Threading stuff -- //
+        private static void DoEnetServerWork()
+        {
+            Console.WriteLine("ENet Server Worker: Arrived");
+            byte[] dummyDataArray = System.Text.Encoding.UTF8.GetBytes(dummyData);
+
+            using (Host server = new Host())
+            {
+                Address address = new Address();
+
+                address.Port = (ushort)port;
+                server.Create(address, maxClients, maxChannels);
+
+                Event netEvent;
+
+                while (!killSwitch)
+                {
+                    bool polled = false;
+
+                    while (!polled)
+                    {
+                        if (server.CheckEvents(out netEvent) <= 0)
+                        {
+                            if (server.Service(15, out netEvent) <= 0)
+                                break;
+
+                            polled = true;
+                        }
+
+                        switch (netEvent.Type)
+                        {
+                            case EventType.None:
+                                break;
+
+                            case EventType.Connect:
+                                Console.WriteLine($"ENet Server Worker: Client connected - ID: {netEvent.Peer.ID}, IP: {netEvent.Peer.IP}");
+                                break;
+
+                            case EventType.Disconnect:
+                                Console.WriteLine($"ENet Server Worker: Client disconnected - ID: {netEvent.Peer.ID}, IP: {netEvent.Peer.IP}");
+                                break;
+
+                            case EventType.Timeout:
+                                Console.WriteLine($"ENet Server Worker: Client timeout - ID: {netEvent.Peer.ID}, IP: {netEvent.Peer.IP}");
+                                break;
+
+                            case EventType.Receive:
+                                Console.WriteLine($"ENet Server Worker: Packet received from peer {netEvent.Peer.ID} ({netEvent.Peer.IP}:{netEvent.Peer.Port}); Ch. {netEvent.ChannelID}, {netEvent.Packet.Length} bytes");
+                                netEvent.Packet.Dispose();
+
+                                // Immediately counter the rick roll, with another rick roll packet.
+                                Packet spam = default(Packet);
+                                spam.Create(dummyDataArray, PacketFlags.Reliable);
+
+                                netEvent.Peer.Send(0, ref spam);
+                                break;
+                        }
+                    }
+                }
+
+                server.Flush();
+            }
+
+            Console.WriteLine("ENet Server Worker: Departed");
+        }
+
+        private static void DoEnetClientWork()
+        {            
+            Console.WriteLine("ENet Client Worker: Arrived");
+            byte[] dummyDataArray = System.Text.Encoding.UTF8.GetBytes(dummyData);
+
+            using (Host client = new Host())
+            {
+                Address address = new Address();
+
+                address.SetHost(serverAddress);
+                address.Port = (ushort)port;
+                client.Create();
+
+                Console.WriteLine("ENet Client Worker: Attempting connection.");
+                Peer peer = client.Connect(address);
+
+                Event netEvent;
+
+                while (!killSwitch)
+                {
+                    bool polled = false;
+
+                    while (!polled)
+                    {
+                        if (client.CheckEvents(out netEvent) <= 0)
+                        {
+                            if (client.Service(15, out netEvent) <= 0)
+                                break;
+
+                            polled = true;
+                        }
+
+                        switch (netEvent.Type)
+                        {
+                            case EventType.None:
+                                break;
+
+                            case EventType.Connect:
+                                Console.WriteLine($"ENet Client Worker: Connected to server ({netEvent.Peer.IP}:{netEvent.Peer.Port})");
+
+                                // Immediately send a rick roll.
+                                Packet rickrolld = default(Packet);
+                                rickrolld.Create(dummyDataArray, PacketFlags.Reliable);
+
+                                peer.Send(0, ref rickrolld);
+                                break;
+
+                            case EventType.Disconnect:
+                                Console.WriteLine("ENet Client Worker: Disconnected from server");
+                                break;
+
+                            case EventType.Timeout:
+                                Console.WriteLine("ENet Client Worker: Connection timeout");
+                                break;
+
+                            case EventType.Receive:
+                                Console.WriteLine($"ENet Client Worker: Packet received from server ({netEvent.Peer.IP}:{netEvent.Peer.Port}); Ch. {netEvent.ChannelID}, {netEvent.Packet.Length} bytes");
+                                netEvent.Packet.Dispose();
+
+                                // Uno reverse card
+                                Packet spam = default(Packet);
+                                spam.Create(dummyDataArray, PacketFlags.Reliable);
+
+                                peer.Send(0, ref spam);
+                                break;
+                        }
+                    }
+                }
+
+                client.Flush();
+            }
+
+            Console.WriteLine("ENet Client Worker: Departed");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # ENet-Tester
-NET Core-based Testing Application for native ENet libraries.
+
+[![Ko-Fi](https://img.shields.io/badge/Donate-Ko--Fi-red)](https://ko-fi.com/coburn) 
+[![PayPal](https://img.shields.io/badge/Donate-PayPal-blue)](https://paypal.me/coburn64)
+![MIT Licensed](https://img.shields.io/badge/license-MIT-green.svg)
+
+## What is this?
+ENet-Tester is a NET Core-based Testing Application for native ENet libraries. 
+It is used internally to identify if [a fork of ENet-CSharp](http://github.com/SoftwareGuy/ENet-CSharp) was failing to operate correctly.
+
+The testing application is crude, rushed and pretty jank, however it sends a endless loop of the first few lines of *Never Gonna Give You Up* by Rick Ansley back and forth.
+This was deemed sufficient to ensure that the sending pipeline of ENet was functioning correctly. Right now the application is hard-coded to only allow up to 50 clients out of
+the 4096 theortical maximum that ENet supports - this is to save ENet from looping over empty Peer slots.
+
+## How to use?
+
+**Server Mode** spawns a ENet Reliable UDP server instance, listening for client connections on an IP address and port that you specify.
+
+**Client Mode** spawns a ENet client connection to the server IP address and port specified.
+
+To use, you need to download the repository, and compile the application. Once compiled, invoke the application executable like so:
+
+```
+ENetTester.exe [server/client] [ip] [port]
+```
+**Make sure** you slap a release of the ENet for your architecture from, for example, my [ENet-CSharp releases](https://github.com/SoftwareGuy/ENet-CSharp/releases)
+next to the application. Or **you will** get an "DllNotFound" exception.
+
+### Server and Client Connection Examples
+
+If you want a server on 192.168.88.200 and port 8008, you would do:
+```
+ENetTester.exe server 192.168.88.200 8008
+```
+
+If you wanted a client to connect to a existing instance on 192.168.88.220 port 1337, you would do:
+```
+ENetTester.exe client 192.168.88.220 1337
+```
+
+If you fail to provide the needed information, such as server or client mode and IP Address, the application will not start and instead will show some
+helpful usage information like so:
+
+```
+ENet Testing Application
+Report bugs and submit PRs at https://github.com/SoftwareGuy/ENet-Tester
+
+This application is licensed under the MIT license
+and comes with no warranty. Read the license file.
+
+Command Line:
+ERROR: Bad usage of this application.
+Usage: ENetTester [server/client] [address] [port]
+Where:
+[server/client] refers to the mode you want. You need to specify either server or client.
+[address] is the IPv4/IPv6 address you wish to bind the server to or connect the client to.
+[port] is the port you wish to bind or connect to.
+```
+
+## Todo List
+
+-	User-configurable peer count
+-	Better argument on the commandline parsing
+-	Less console "received packet" spam, maybe duration, packets recieved in and sent out, total bytes
+-	Less rickroll
+
+## I have a problem.
+
+Get a solution by opening a Issue ticket. Be verbose about your problem though and provide info like your operating system, how it broke, etc. 
+If you don't do this then I will not be able to give a timely reply to your ticket. Help me help you by providing all the information I need.
+
+## License
+Released under MIT license, read LICENSE.md.
